372320b8f611b33f5b07a70df6c485ed696001bb
egor.dranischnikow <egor.dranischnikow@googlemail.com>
2016-06-28 15:27:35 +0200

    introducing assembler code class

--- src/rmclib/AMD64Compiler.py
+++ src/rmclib/AMD64Compiler.py
@@ -1,4 +1,5 @@
 
+from ascode import AssemblerCode
 
 class AMD64CompilerException(Exception):
     pass
@@ -14,7 +15,11 @@ class AMD64Compiler:
         self.out=open(output_file, 'w')
         self.out.write('\t.file\t"{0}"\n'.format(input_file))
         self.out.write('\t.text\n')
+    
+    def write_assembler_code(self, assembler_code):
+        assembler_code.write_to_file(self.out)
         
+            
     def register_global_function(self, mangled_fun_name, fun_body):
         if mangled_fun_name in self.mangled_funs:
             raise AMD64CompilerException("redefinition of function "+mangled_fun_name)
@@ -24,37 +29,41 @@ class AMD64Compiler:
         self.mangled_funs.add(mangled_fun_name)
         
         #prolog:
-        self.out.write('\t.globl\t{0}\n'.format(mangled_fun_name))
-        self.out.write('\t.type\t{0}, @function\n'.format(mangled_fun_name))
-        self.out.write('{0}:\n'.format(mangled_fun_name))
-        self.out.write('.LFB{0}:\n'.format(fun_id))
+        code=AssemblerCode()
+        code.append_tabbed_line('.globl\t{0}'.format(mangled_fun_name))
+        code.append_tabbed_line('.type\t{0}, @function'.format(mangled_fun_name))
+        code.append_code_line('{0}:'.format(mangled_fun_name))
+        code.append_code_line('.LFB{0}:'.format(fun_id))
         
         #inner_part
-        self.out.write('\t.cfi_startproc\n')
-        self.out.write('\tpushq\t%rbp\n')
-        self.out.write('\t.cfi_def_cfa_offset 16\n')
-        self.out.write('\t.cfi_offset 6, -16\n')
-        self.out.write('\tmovq\t%rsp, %rbp\n')
-        self.out.write('\t.cfi_def_cfa_register 6\n')
+        code.append_tabbed_line('.cfi_startproc')
+        code.append_tabbed_line('pushq\t%rbp')
+        code.append_tabbed_line('.cfi_def_cfa_offset 16')
+        code.append_tabbed_line('.cfi_offset 6, -16\n')
+        code.append_tabbed_line('movq\t%rsp, %rbp\n')
+        code.append_tabbed_line('.cfi_def_cfa_register 6\n')
 	    
 	    #error if something wrong
-        self.out.write('\tmovl\t$1, %eax\n')
+        code.append_tabbed_line('movl\t$1, %eax')
 	    
 	    #code of the function	
         self.parser.parse(fun_body)
         
         #no errors
-        self.out.write('\tmovl\t$0, %eax\n')
+        code.append_tabbed_line('movl\t$42, %eax')
         
         #cleaning up
-        self.out.write('\tpopq\t%rbp\n')
-        self.out.write('\t.cfi_def_cfa 7, 8\n')
-        self.out.write('\tret\n')
-        self.out.write('\t.cfi_endproc\n')
+        code.append_tabbed_line('popq\t%rbp')
+        code.append_tabbed_line('.cfi_def_cfa 7, 8')
+        code.append_tabbed_line('ret')
+        code.append_tabbed_line('.cfi_endproc')
         
         #epilog:
-        self.out.write('.LFE{0}:\n'.format(fun_id))
-        self.out.write('\t.size\t{0}, .-{0}\n'.format(mangled_fun_name))
+        code.append_code_line('.LFE{0}:'.format(fun_id))
+        code.append_tabbed_line('.size\t{0}, .-{0}'.format(mangled_fun_name))
+        
+        #flush to the file:
+        self.write_assembler_code(code)
             
     def emit_main(self, inner_code):
         self.register_global_function("main", inner_code)
---
+++ src/rmclib/ascode.py
@@ -0,0 +1,19 @@
+
+class AssemblerCode:
+
+    def __init__(self):
+        self.code=[]
+        
+    def append_code_line(self, line):
+        self.code.append(line+'\n')
+    
+    def append_tabbed_line(self, line):
+        self.append_code_line('\t'+line)
+        
+    def extend_code(self, assembler_lines):
+        self.code.extend(assembler_lines.code)
+            
+    def write_to_file(self, f):
+        f.writelines(self.code)
+
+	
