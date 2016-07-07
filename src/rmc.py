8676a6e38178019df4ba580679d904d3bf9a5501
egor.dranischnikow <egor.dranischnikow@googlemail.com>
2016-07-07 06:54:25 +0200

    using rm runtime and not encoding it into the resulting assembler file

--- src/rmc.py
+++ src/rmc.py
@@ -1,3 +1,12 @@
+##finding path to run_time
+
+import sys
+import os
+
+path_to_rt=os.path.join(os.path.dirname(sys.argv[0]), "rmrt")
+
+
+
 import argparse
 
 parser = argparse.ArgumentParser(description='Register Machine Compiler')
@@ -8,12 +17,14 @@ parser.add_argument('-o', type=str,
                     
 args = parser.parse_args()
 
-import os
 
 input_name=os.path.basename(args.c)
 path, ext=os.path.splitext(args.c)
 asm_file=path+".s"
+obj_file=path+".o"
 
+
+#compile rmprogram
 from rmclib.RMParser import RMParser as Parser
 from rmclib.AMD64Compiler import AMD64Compiler as Compiler
 
@@ -22,11 +33,29 @@ compiler=Compiler(asm_file, input_name, parser)
 
 parser.compile_file(compiler)
 
-#start  gcc:
-import subprocess
-command=['gcc', asm_file,'-o', args.o]
-subprocess.call(command)
 
-#print"result:", subprocess.check_output(command, stderr=subprocess.STDOUT)
+
+#assembly runtime:
+command_list=[];
+rt_files=["atouq", "error_exit", "print_newline", "startup", "uqtoa"]
+for rt_file in rt_files:
+    command_list.append(['as', "-I"+path_to_rt, os.path.join(path_to_rt, rt_file)+".s", '-o', rt_file+".o"])
+    
+
+
+#assembly rmprog
+command_list.append(['as', asm_file, '-o', obj_file])
+
+#link everything
+command_list.append(['ld', '-o', args.o, obj_file]+[f+'.o' for f in rt_files])
+
+
+#invoke the program:
+import subprocess
+for command in command_list:
+   print "calling:", ' '.join(command)
+   subprocess.call(command)
+
+  
 
  
--- src/rmclib/AMD64Compiler.py
+++ src/rmclib/AMD64Compiler.py
@@ -33,33 +33,15 @@ class AMD64Compiler:
         code.append_tabbed_line('.globl\t{0}'.format(mangled_fun_name))
         code.append_tabbed_line('.type\t{0}, @function'.format(mangled_fun_name))
         code.append_code_line('{0}:'.format(mangled_fun_name))
-        code.append_code_line('.LFB{0}:'.format(fun_id))
-        
-        #inner_part
-        code.append_tabbed_line('.cfi_startproc')
-        code.append_tabbed_line('pushq\t%rbp')
-        code.append_tabbed_line('.cfi_def_cfa_offset 16')
-        code.append_tabbed_line('.cfi_offset 6, -16')
-        code.append_tabbed_line('movq\t%rsp, %rbp')
-        code.append_tabbed_line('.cfi_def_cfa_register 6')
-	    
-	    #flush to the file:
         self.write_assembler_code(code)
+         
+        #inner_part
         
 	    #code of the function	
         self.parser.parse(fun_body)
-      
-      
+
         code=AssemblerCode()
-        #cleaning up
-        code.append_tabbed_line('popq\t%rbp')
-        code.append_tabbed_line('.cfi_def_cfa 7, 8')
         code.append_tabbed_line('ret')
-        code.append_tabbed_line('.cfi_endproc')
-        
-        #epilog:
-        code.append_code_line('.LFE{0}:'.format(fun_id))
-        code.append_tabbed_line('.size\t{0}, .-{0}'.format(mangled_fun_name))
         
         #flush to the file:
         self.write_assembler_code(code)
@@ -69,29 +51,7 @@ class AMD64Compiler:
             
     def emit_main(self):
         inner_code=AssemblerCode()   
-        #no errors -> result is 42
-        inner_code.append_tabbed_line('movl\t$42, %eax')
-        inner_code.append_comment('we use 12 bytes on the stack, rsp must be multiple of 16')    
-        inner_code.append_tabbed_line('subq\t$16, %rsp')
-        
-        inner_code.append_comment('remember the number of arguments (int)') 
-        inner_code.append_tabbed_line('movl\t%edi, -4(%rbp)')
-        inner_code.append_comment('remember the arguments')   
-        inner_code.append_tabbed_line('movq\t%rsi, -16(%rbp)')
-        
-        inner_code.append_comment('put the value of the argv[1] into rdi - first argument of c2i')  
-        inner_code.append_tabbed_line('movq\t-16(%rbp), %rax')
-        inner_code.append_tabbed_line('addq\t$8, %rax')
-        inner_code.append_tabbed_line('movq\t(%rax), %rax')
-        inner_code.append_tabbed_line('movq\t%rax, %rdi')
-              
-        inner_code.append_comment('call c2i') 
-        inner_code.append_tabbed_line('call\tc2i')
-        
-        inner_code.append_comment('free the frame') 
-        inner_code.append_tabbed_line('movq\t%rbp, %rsp')
-           
-        self.register_global_function("main", inner_code)
+        self.register_global_function("rmprogram", inner_code)
         
         
         
@@ -99,37 +59,6 @@ class AMD64Compiler:
     def close_output(self):
         self.out.close()
         
-        
-        
-        
-        
-    def c2i_assembler_function(self):
-        code=AssemblerCode()
-        
-        code.append_tabbed_line('movq\t%rdi, -24(%rbp)')
-        code.append_tabbed_line('movl\t$0, -4(%rbp)')
-        code.append_tabbed_line('jmp\t.L2')
-        code.append_code_line('.L3:')
-        code.append_tabbed_line('movl\t-4(%rbp), %edx')
-        code.append_tabbed_line('movl\t%edx, %eax')
-        code.append_tabbed_line('sall\t$2, %eax')
-        code.append_tabbed_line('addl\t%edx, %eax')
-        code.append_tabbed_line('addl\t%eax, %eax')
-        code.append_tabbed_line('movl\t%eax, %edx')
-        code.append_tabbed_line('movq\t-24(%rbp), %rax')
-        code.append_tabbed_line('movzbl\t(%rax), %eax')
-        code.append_tabbed_line('movsbl\t%al, %eax')
-        code.append_tabbed_line('subl\t$48, %eax')
-        code.append_tabbed_line('addl\t%edx, %eax')
-        code.append_tabbed_line('movl\t%eax, -4(%rbp)')
-        code.append_tabbed_line('addq\t$1, -24(%rbp)')
-        code.append_code_line('.L2:')
-        code.append_tabbed_line('movq\t-24(%rbp), %rax')
-        code.append_tabbed_line('movzbl\t(%rax), %eax')
-        code.append_tabbed_line('testb\t%al, %al')
-        code.append_tabbed_line('jne\t.L3')
-        code.append_tabbed_line('movl\t-4(%rbp), %eax')
-        
-        return code
+
 
 	
