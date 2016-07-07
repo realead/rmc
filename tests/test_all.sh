4889575a4b729808dc3f0eab8e73e375d954ea3e
egor.dranischnikow <egor.dranischnikow@googlemail.com>
2016-07-07 05:35:20 +0200

    improving test_all.sh script, so test cases can be also checked separatedly

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,12 +1,30 @@
+#options
 
 
-for d in *; do
-    if [ -d "$d" ] && [ $d != "tools" ]; then
-         cd "$d"
-         echo "Test $d:"
-         sh test_me.sh
-         cd ..
-    fi
-done
+TEST_CASE=${1:-all}
+KEEP=$2
+
+
+######## RUN ###################
+
+#$1 directory
+#$2 whether to keep
+test_case(){
+     cd "$1"
+     echo "Test $1:"
+     sh test_me.sh $KEEP
+     cd ..
+}
+
+
+if [ $TEST_CASE = "all"  ]; then
+    for d in *; do
+        if [ -d "$d" ] && [ $d != "tools" ]; then
+             test_case $d
+        fi
+    done
+else
+   test_case $TEST_CASE
+fi 
 
 
