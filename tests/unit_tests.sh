858c1df12af93d2a2fb7ccf88d768405bba75dcd
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2017-06-19 21:54:11 +0200

    make it possible to choose differnt test configuration for unit_tests

--- tests/unit_tests.sh
+++ tests/unit_tests.sh
@@ -1,9 +1,14 @@
 
 PATTERN=${1:-*}
+if [ "$PATTERN" = "all" ]; then
+   PATTERN=*
+fi
+
+TESTCASE=${2:-.}
 
 
 export PYTHONPATH="${PYTHONPATH}:exetest:."
 export RMI=../src/rmi.py
 export RMC=../src/rmc.py
-python -m unittest discover -p "test_$PATTERN.py" -v
+python -m unittest discover "$TESTCASE" -p "test_$PATTERN.py" -v
 
